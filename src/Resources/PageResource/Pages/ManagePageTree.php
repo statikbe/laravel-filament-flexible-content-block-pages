680297599e52ebd645f99b070d1adc45b2343dfa
sten <180665+sten@users.noreply.github.com>
2025-09-23 21:57:25 +0000

    Fix styling

--- src/Resources/PageResource/Pages/ManagePageTree.php
+++ src/Resources/PageResource/Pages/ManagePageTree.php
@@ -78,6 +78,6 @@ class ManagePageTree extends TreePage
     public function getTreeRecordIcon(?Model $record = null): ?string
     {
         /** @var HasPageAttributes $record */
-        return $record?->isPublished() ? null : 'heroicon-o-eye-slash' ;
+        return $record?->isPublished() ? null : 'heroicon-o-eye-slash';
     }
 }
