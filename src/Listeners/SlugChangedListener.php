12588941bdfdc1f864ebb6f18e5bfb1365003fc8
Sten Govaerts <sten.govaerts@gmail.com>
2025-09-25 22:33:02 +0200

    Avoid circular redirect references when slug changes

--- src/Listeners/SlugChangedListener.php
+++ src/Listeners/SlugChangedListener.php
@@ -3,6 +3,7 @@
 namespace Statikbe\FilamentFlexibleContentBlockPages\Listeners;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Statikbe\FilamentFlexibleContentBlockPages\Models\Page;
 use Statikbe\FilamentFlexibleContentBlockPages\Models\Redirect;
 use Statikbe\FilamentFlexibleContentBlocks\Events\SlugChanged;
@@ -33,16 +34,41 @@ class SlugChangedListener
                     $oldUrlPath = parse_url($oldUrl, PHP_URL_PATH);
                     $newUrlPath = parse_url($newUrl, PHP_URL_PATH);
 
-                    $redirectExists = Redirect::where('old_url', $oldUrlPath)
-                        ->where('new_url', $newUrlPath)
-                        ->exists();
+                    try {
+                        DB::beginTransaction();
 
-                    if (! $redirectExists) {
-                        $redirect = new Redirect;
-                        $redirect->old_url = $oldUrlPath;
-                        $redirect->new_url = $newUrlPath;
-                        $redirect->status_code = Response::HTTP_MOVED_PERMANENTLY;
-                        $redirect->save();
+                        // clean up old redirects & avoid circular references:
+                        // Rule 1: Delete records where $newUrlPath matches existing old_url.
+                        // This means that the starting point of the redirect, has been recreated.
+                        Redirect::where('old_url', $newUrlPath)->delete();
+
+                        // Rule 2: Update records where $oldUrlPath matches existing new_url.
+                        // This means that old slug, was already the destination of an existing redirect, so to avoid
+                        // hopping multiple redirects, we update the existing redirect to the new slug.
+                        Redirect::where('new_url', $oldUrlPath)->update(['new_url' => $newUrlPath]);
+
+                        // Rule 3: Delete self-redirects
+                        // Maybe we have created cases were the source and destination is the same.
+                        Redirect::whereColumn('old_url', 'new_url')->delete();
+
+                        // Rule 4: Add a new redirect if it doesn't exist yet
+                        $redirectExists = Redirect::where('old_url', $oldUrlPath)
+                            ->where('new_url', $newUrlPath)
+                            ->exists();
+
+                        if (!$redirectExists) {
+                            $redirect = new Redirect;
+                            $redirect->old_url = $oldUrlPath;
+                            $redirect->new_url = $newUrlPath;
+                            $redirect->status_code = Response::HTTP_MOVED_PERMANENTLY;
+                            $redirect->save();
+                        }
+
+                        DB::commit();
+                    }
+                    catch (\Exception $e) {
+                        report($e);
+                        DB::rollBack();
                     }
                 }
             }
